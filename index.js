14848503be2619b634c09a577ebe18db222c9786
prabhu_b <prabhubarathi@gmail.com>
2018-08-21 13:51:14 -0400

    v2

--- index.js
+++ index.js
@@ -82,8 +82,8 @@ controller.hears(['Create 3 VKE Clusters in devB, folder interestwebsite called 
       axios.post('https://api.vke.cloud.vmware.com//v1/orgs/bfd7a751-b2f5-43a3-b2b3-0c82d6896aee/clusters?region=us-west-2', {
           "name": `${ipadd}`+`-${built_clusters}`,
           "displayName": `${ipadd}`+`-${built_clusters}`,
-          "folderName": "devB",
-          "projectName": "interestwebsite",
+          "folderName": "InterestWebsite",
+          "projectName": "devB",
           "serviceLevel": "DEVELOPER",
           "networking": {
             "networkingTenancy": "SHARED"
